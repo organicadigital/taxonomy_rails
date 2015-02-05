e2ca86705dda9b80444bb7189b70682548bd0205
Rodrigo Willrich <rodrigo.willrich@hotmail.com>
2015-02-05 11:16:10 -0200

    Add touch to Taxon belongs_to Taxonomy association

--- app/models/taxon.rb
+++ app/models/taxon.rb
@@ -1,6 +1,6 @@
 class Taxon < ActiveRecord::Base
 
-  belongs_to :taxonomy, inverse_of: :taxons
+  belongs_to :taxonomy, inverse_of: :taxons, touch: true
 
   validates :name, presence: true
   validates :taxonomy_id, presence: true
