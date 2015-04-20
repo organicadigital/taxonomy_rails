fe75e16d6ef49112d3aa3006ae4977a66faadef5
Luiz Fernando Rodrigues <fernahh@gmail.com>
2015-04-20 15:46:20 -0300

    Remove polymorphic validations

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -4,7 +4,4 @@ class Classification < ActiveRecord::Base
   belongs_to :classifiable, polymorphic: true, inverse_of: :classification
 
   validates :taxon_id, presence: true
-  validates :classifiable_id, presence: true
-  validates :classifiable_type, presence: true
-
 end
